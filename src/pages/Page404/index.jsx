aa2ae6ec71abed1292dd4b8472a6cf0be07d36bf
Napoli06 <davi.napoli@etec.sp.gov.br>
2025-06-05 10:38:37 -0300

    primeiro commit

---
+++ src/pages/Page404/index.jsx
@@ -0,0 +1,17 @@
+import Container from "../../components/Container"
+import Footer from "../../components/Footer"
+import Header from "../../components/Header"
+
+function Page404() {
+    return (
+        <>
+            <Header />
+                <Container>
+                    <h1>ERRO 404. PÁGINA NÃO ENCONTRADA</h1>
+                </Container>
+            <Footer />
+        </>
+    )
+}
+
+export default Page404
