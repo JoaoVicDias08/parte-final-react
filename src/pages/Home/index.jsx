ec3181bf8751da721eec1f2a40303b8617b29da7
Napoli06 <davi.napoli@etec.sp.gov.br>
2025-06-05 11:34:41 -0300

    tirei o import do svg

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,4 @@
 import styles from './Home.module.css'
-import imageFigure from '/image-1.svg'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import { Link } from 'react-router-dom'
